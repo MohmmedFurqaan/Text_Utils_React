1c768aa1cc3bdd8882cd524cb077d961252c90fc
MohmmedFurqaan <mmftdec@gmail.com>
2025-12-27 15:15:43 +0530

    (ADDED) Capitalize the letter in the text box

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,18 @@ import TextForm from "./TextForm"
 export default function App() {
   return (
     <>
-        {/* for the Navbar */}
-        <NavBar
-        nav_info={{
-            title: "TextUtils",
-            about_text: "About",
-        }}
-        />
+      {/* Navbar component */}
+      <NavBar
+      nav_info={{
+          title: "TextUtils",
+          about_text: "About",
+      }}
+      />
 
-        {/* text component */}
-        <div className="container mt-5">
-            <TextForm heading={"Enter the text to analyze"} />
-        </div>
+      {/* text component */}
+      <div className="container">
+          <TextForm heading={"Enter the text to analyze"} />
+      </div>
 
         
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,22 +1,49 @@
-import Button from "./Button";
+import React, { useState } from 'react'
+
 
 export default function TextForm({ heading }) {
-  return (
-    <>
-      <div className="container pb-3 my-4">
-        <label htmlFor="myBox" className="form-label">
-          {heading}
-        </label>
-        <textarea
-          className="form-control"
-          id="myBox"
-          rows="9"
-        ></textarea>
-      </div>
-      <div className="container">
-        <Button content={"CAPITALIZE"} />
-      </div>
-      
-    </>
-  );
+
+    // declaring constants for the state 
+    const [text, setText] = useState("");
+
+    // handle clicking
+    const HandleUpclick = ()=>{
+        let capital_text = text.toUpperCase();
+        setText(capital_text);
+    }
+
+    // handle on change
+    const HandleOnChange = (event)=> {
+        // to allow entery in the text box
+        setText(event.target.value)
+    }
+
+
+    
+    return (        
+        <>
+            <div>
+                <h1>{heading}</h1>
+                <div className="mb-3">
+                   {/* <label htmlFor="myBox" className="form-label">{heading}</label> */}
+                    <textarea
+                    className="form-control"
+                    value={text}
+                    onChange={HandleOnChange}
+                    id="myBox"
+                    rows="8"
+                    ></textarea> 
+                </div>
+
+                {/* Button for capitalize */}
+                <button className="btn btn-primary" onClick={HandleUpclick}>CAPITALIZE</button>
+                
+            </div>
+            
+
+            
+
+
+        </>
+    );
 }
